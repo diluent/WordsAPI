6d9726c77298e976193ec90254fa29733d44a59e
diluent <kotikdv@gmail.com>
2016-09-19 01:45:10 +0300

    experiments

--- server.js
+++ server.js
@@ -1,18 +1,96 @@
-var express         = require('express');
-var path            = require('path'); // модуль для парсинга пути
-var app = express();
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var http       = require('http');
 
-app.use(express.favicon()); // отдаем стандартную фавиконку, можем здесь же свою задать
-app.use(express.logger('dev')); // выводим все запросы со статусами в консоль
-app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах
-app.use(express.methodOverride()); // поддержка put и delete
-app.use(app.router); // модуль для простого задания обработчиков путей
-app.use(express.static(path.join(__dirname, "public"))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/api', function (req, res) {
-    res.send('API is running');
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+
+    res.json({message: 'run'})
+    requestOxford('hello', console.log);//{ message: 'hooray! welcome to our api!' });   
 });
 
-app.listen(1337, function(){
-    console.log('Express server listening on port 1337');
+router.route('/bears')
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .get(function(req, res) {
+            res.json({ message: 'Bear created!' });
+    });
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send(err.stack);
 });
+
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+function requestOxford(word, response) {
+  var post_options = {
+      host: 'ya.ru',//'od-api-2445581300291.apicast.io',
+      //port: '443',
+      //path: '/api/v1/entries/en/' + word +'translations=ru',
+      method: 'GET',
+      headers: {
+          //'Accept': 'application/json',
+          //'app_id': '71abeb5f',
+          //'app_key': 'd9def23eb55db2c6dad778500e9a233e',
+          
+          //'Content-Length': Buffer.byteLength(post_data),
+          //'Content-Type': 'application/x-www-form-urlencoded',
+      }
+  };
+
+//     var post_data = querystring.stringify({
+//       'compilation_level' : 'ADVANCED_OPTIMIZATIONS',
+//       'output_format': 'json',
+//       'output_info': 'compiled_code',
+//         'warning_level' : 'QUIET',
+//         'js_code' : codestring
+//   });
+
+ var post_req = http.request(post_options, function(res) {
+     console.log('it is happening');
+     //console.log(res);
+
+     var result = '';
+      //res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          console.log('chunk: ' + chunk);
+          
+          result += chunk;
+      });
+
+      res.on('end', function () {
+          console.log('Response: ' + result);
+          response({res: 'oops'});
+        });
+
+        // res.on('error', function() {
+        //   response({error: 'error'});
+        // })
+      
+  });
+
+//post_req.write("hello world!");
+post_req.end();
+
+  // post the data
+  //post_req.write(post_data);
+  //post_req.end();
+}
