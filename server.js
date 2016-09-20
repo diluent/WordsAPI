a1aa815d09084de7e190bff3597023f0b256819c
diluent <kotikdv@gmail.com>
2016-09-21 00:14:16 +0300

    experiments

--- server.js
+++ server.js
@@ -15,11 +15,7 @@ var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-
-    res.json({message: 'run'})
-    requestOxford('hello', console.log);//{ message: 'hooray! welcome to our api!' });   
-});
+router.get('/', requestOxford);
 
 router.route('/bears')
     // create a bear (accessed at POST http://localhost:8080/api/bears)
@@ -40,16 +36,19 @@ app.use(function(err, req, res, next) {
 app.listen(port);
 console.log('Magic happens on port ' + port);
 
-function requestOxford(word, response) {
+function requestOxford(req, res) {
+    var word = 'hello';
   var post_options = {
-      host: 'ya.ru',//'od-api-2445581300291.apicast.io',
-      //port: '443',
-      //path: '/api/v1/entries/en/' + word +'translations=ru',
+      //host: 'https://od-api-2445581300291.apicast.io',
+    //   host: 'od-api-demo.oxforddictionaries.com',
+        host: 'google.ru',
+    //   port: '443',
+    //   path: '/api/v1/entries/en/' + word +'translations=ru',
       method: 'GET',
       headers: {
-          //'Accept': 'application/json',
-          //'app_id': '71abeb5f',
-          //'app_key': 'd9def23eb55db2c6dad778500e9a233e',
+           'Accept': 'application/json',
+           'app_id': '71abeb5f',
+           'app_key': 'd9def23eb55db2c6dad778500e9a233e',
           
           //'Content-Length': Buffer.byteLength(post_data),
           //'Content-Type': 'application/x-www-form-urlencoded',
@@ -64,27 +63,28 @@ function requestOxford(word, response) {
 //         'js_code' : codestring
 //   });
 
- var post_req = http.request(post_options, function(res) {
-     console.log('it is happening');
-     //console.log(res);
-
+ var post_req = http.request(post_options, function(_res) {
      var result = '';
       //res.setEncoding('utf8');
-      res.on('data', function (chunk) {
+      _res.on('data', function (chunk) {
           console.log('chunk: ' + chunk);
           
           result += chunk;
       });
 
-      res.on('end', function () {
-          console.log('Response: ' + result);
-          response({res: 'oops'});
+      _res.on('end', function () {
+          //console.log('Response: ' + response);
+          res.json({res: result});
         });
 
         // res.on('error', function() {
         //   response({error: 'error'});
         // })
       
+  });
+
+  post_req.on('error', function(err) {
+      res.json({error:err})
   });
 
 //post_req.write("hello world!");
